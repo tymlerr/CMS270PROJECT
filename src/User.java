3cfe9eda8bf470d6f5c3b364b0b0827131399423
tymlerr <110439125+tymlerr@users.noreply.github.com>
2025-10-28 10:58:43 -0400

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,4 +1,12 @@
 
-public class User {
+public abstract class User {
+	protected String user;
+	protected String password;
+	protected String name;
+	
+	// constructor
+	public User(String user, String password, String name) {
+		
+	}
 
 }
