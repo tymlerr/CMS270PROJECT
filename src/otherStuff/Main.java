34d80a324e473b54da2f65a2ec168bdbc532e8a4
Robert H <rharvie.jr@gmail.com>
2025-11-19 13:50:59 -0500

    Adding Main and Test data

--- src/otherStuff/Main.java
+++ src/otherStuff/Main.java
@@ -1,10 +1,14 @@
 package otherStuff;
 
+import gui.*;
+
 public class Main {
-
 	public static void main(String[] args) {
-		
-
+	    // Populate dummy accounts
+	    TestData.populate();
+	
+	    // Launch GUI
+	    new MainGUI();
 	}
 
 }
