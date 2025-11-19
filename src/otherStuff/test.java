b96abfdd8473a12bd43b30cbee5cbd8fed2d5d50
Robert H <rharvie.jr@gmail.com>
2025-11-19 12:56:23 -0500

    Delete test.java

--- src/otherStuff/test.java
+++
@@ -1,8 +0,0 @@
-package otherStuff;
-public class test {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
-        System.out.println("tylers input");
-        System.out.println("Hello R");
-    }
-}
